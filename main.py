1ce4056b7c01a511d5799a57a9f3f328b3301737
Muhammed Fazin <86030159+M-fazin@users.noreply.github.com>
2021-12-22 10:22:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,10 +55,10 @@ async def sng(bot, message):
         chat_id = message.from_user.id
         rpl = lyrics(song)
         await hy.delete()
-        if 'error' not in search(song):
+        try:
                 await hy.delete()
                 await Ek.send_message(chat_id, text = rpl, reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Dev 🔗 ", url = f"github.com/M-fazin")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]]))
-        else:
+        except Exception as e:
         	await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Developer", url = f"github.com/M-fazin")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]]))
 
 
